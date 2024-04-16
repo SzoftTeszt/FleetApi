cd68a4f5d605274c843b4017f1b0e35fab09a7ec
Nagy János <nagy@zold.lan>
2024-04-16 20:22:11 +0200

    Add project files.

---
+++ FleetApi/Data/FleetApiContext.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.EntityFrameworkCore;
+using FleetApi.Models;
+
+namespace FleetApi.Data
+{
+    public class FleetApiContext : DbContext
+    {
+        public FleetApiContext (DbContextOptions<FleetApiContext> options)
+            : base(options)
+        {
+        }
+
+        public DbSet<FleetApi.Models.Driver> Driver { get; set; } = default!;
+    }
+}
---
+++ FleetApi/Program.cs
@@ -0,0 +1,36 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.DependencyInjection;
+using FleetApi.Data;
+var builder = WebApplication.CreateBuilder(args);
+builder.Services.AddDbContext<FleetApiContext>(options =>
+    options.UseSqlServer(builder.Configuration.GetConnectionString("FleetApiContext") ?? throw new InvalidOperationException("Connection string 'FleetApiContext' not found.")));
+
+// Add services to the container.
+
+builder.Services.AddControllers();
+// Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
+builder.Services.AddEndpointsApiExplorer();
+builder.Services.AddSwaggerGen();
+
+var app = builder.Build();
+
+// Configure the HTTP request pipeline.
+if (app.Environment.IsDevelopment())
+{
+    app.UseSwagger();
+    app.UseSwaggerUI();
+}
+
+app.UseCors(builder => builder
+       .AllowAnyHeader()
+       .AllowAnyMethod()
+       .AllowAnyOrigin()
+    );
+
+app.UseHttpsRedirection();
+
+app.UseAuthorization();
+
+app.MapControllers();
+
+app.Run();
